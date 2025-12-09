44fb21aa3430e13ab0e58b654a1aa364cb288f89
FN\jianzhong.pi <jianzhongpi@163.com>
2025-12-09 14:31:04 +0800

    Upgrade features

--- plugin/plugins/fh2.go
+++ plugin/plugins/fh2.go
@@ -33,10 +33,10 @@ func NewFH2Adapter() *FH2Adapter {
 }
 
 // 添加设置token的方法
-func (f *FH2Adapter) SetUserToken(token string) {
-	f.mu.Lock()
-	defer f.mu.Unlock()
-	f.xUserToken = token
+func (F *FH2Adapter) SetUserToken(token string) {
+	F.mu.Lock()
+	defer F.mu.Unlock()
+	F.xUserToken = token
 }
 
 // 定义与API响应对应的结构体
@@ -94,16 +94,16 @@ func (F *FH2Adapter) GetprojectList() (string, error) {
 	return string(resp), err
 }
 
-// 获取项目的存储上传凭证
-func (F *FH2Adapter) GetProjectStsToken(projectUuid string) (string, error) {
-	url := fmt.Sprintf("%s/openapi/v0.1/project/sts-token", config.FH2Settings["host"])
+// 获取项目下的设备列表
+func (F *FH2Adapter) GetDeviceList(projectUuid string) (string, error) {
+	url := fmt.Sprintf("%s/openapi/v0.1/project/device", config.FH2Settings["host"])
 	resp, err := F.doRequest(context.Background(), http.MethodGet, url, nil, projectUuid)
 	return string(resp), err
 }
 
-// 获取项目下的设备列表
-func (F *FH2Adapter) GetDeviceList(projectUuid string) (string, error) {
-	url := fmt.Sprintf("%s/openapi/v0.1/project/device", config.FH2Settings["host"])
+// 获取项目的存储上传凭证
+func (F *FH2Adapter) GetProjectStsToken(projectUuid string) (string, error) {
+	url := fmt.Sprintf("%s/openapi/v0.1/project/sts-token", config.FH2Settings["host"])
 	resp, err := F.doRequest(context.Background(), http.MethodGet, url, nil, projectUuid)
 	return string(resp), err
 }
@@ -123,25 +123,121 @@ func (F *FH2Adapter) GetDeviceHms(projectUuid string, deviceSnList string) (stri
 	return string(resp), err
 }
 
-// 获取设备控制权
-
-// 设备控制权释放
-
-// 设备飞行器镜头切换
+// 实时控制指令下发
+// 在航线飞行或者手动飞行过程中，可以调用此命令对飞行器进行实时控制，比如返航，取消返航，飞行任务暂停和恢复等操作。
+//
+//	payLoad := `{
+//					  "device_command": "return_home", // 控制指令，支持的指令有：return_home（返航）、return_specific_home（蛙跳任务指定目标机场返航）、return_home_cancel（取消返航）、flighttask_pause（飞行任务暂停）、flighttask_recovery（飞行任务恢复）
+//					}`
+func (F *FH2Adapter) UpdateDeviceCommand(projectUuid string, deviceSn string, payLoad io.Reader) (string, error) {
+	url := fmt.Sprintf("%s/openapi/v0.1/device/%s/command", config.FH2Settings["host"], deviceSn)
+	resp, err := F.doRequest(context.Background(), http.MethodPost, url, payLoad, projectUuid)
+	return string(resp), err
+}
 
 // 机场相机切换
+// 支持机场相机的切换，可切换舱内和舱外相机。
+// camera_index 和 camera_position 信息可通过获取组织下的设备列表接口下的 data.list.gateway:.camera_list 中获取。
+//
+//	payLoad := `{
+//					  "sn": "1581F6Q8D242100CPWEK", // 设备编码
+//					  "camera_index": "165-0-7",  // 相机索引，0表示舱外相机，1表示舱内相机
+//					  "camera_position": "indoor",  // 相机位置，indoor表示舱内摄像头，outdoor表示舱外摄像头
+//				   }`
+func (F *FH2Adapter) UpdateDeviceChangeCamera(projectUuid string, payLoad io.Reader) (string, error) {
+	url := fmt.Sprintf("%s/openapi/v0.1/device/change-camera", config.FH2Settings["host"])
+	resp, err := F.doRequest(context.Background(), http.MethodPost, url, payLoad, projectUuid)
+	return string(resp), err
+}
 
-// 图传清晰度设置
+// 设备飞行器镜头切换
+// 支持飞行器相机镜头的切换，可切换广角，变焦和红外。
+// 需要先调用控制权获取接口获取飞行器控制权后才能进行飞行器镜头切换。
+// camera_index 和 lens_type 信息可通过获取组织下的设备列表接口下的 data.list.drone:.camera_list 中获取。
+//
+//	payLoad := `{
+//					   "sn": "1581F6Q8D242100CPWEK", // 设备编码
+//					   "camera_index": "81-0-0",  // 相机索引，0表示主相机，1表示辅助相机
+//					   "lens_type": "zoom"   // 镜头类型，支持 "wide"（广角）、"zoom"（变焦）、"ir"（红外）
+//					}`
+func (F *FH2Adapter) UpdateDeviceChangeLens(projectUuid string, payLoad io.Reader) (string, error) {
+	url := fmt.Sprintf("%s/openapi/v0.1/device/change-lens", config.FH2Settings["host"])
+	resp, err := F.doRequest(context.Background(), http.MethodPost, url, payLoad, projectUuid)
+	return string(resp), err
+}
 
-// 实时控制指令下发
+// 获取设备控制权
+// 如果你需要针对特定飞行器设备的负载进行操作，需要先获取该负载设备的控制权，然后对这个负载进行操作。比如对飞行器的某个负载相机的镜头进行切换。
+// payload_index 参数可通过获取设备列表中data.list.drone.camera_list.camera_index传入。
+//
+//	payLoad := `{
+//				   "sn": "1581F6Q8D242100CPWEK", // 设备编码
+//				   "payload_index": ["81-0-0"]  // 负载索引，此参数可通过获取设备列表中data.list.drone.camera_list.camera_index传入
+//				}`
+func (F *FH2Adapter) GetDeviceControl(projectUuid string, payLoad io.Reader) (string, error) {
+	url := fmt.Sprintf("%s/openapi/v0.1/device/control", config.FH2Settings["host"])
+	resp, err := F.doRequest(context.Background(), http.MethodPost, url, payLoad, projectUuid)
+	return string(resp), err
+}
 
-// 获取项目的存储上传凭证
+// 设备控制权释放
+// 释放指定设备的指定负载的控制权。
+// payload_index 参数可通过获取设备列表中data.list.drone.camera_list.camera_index传入。
+//
+//	payLoad := `{
+//				   "sn": "1581F6Q8D242100CPWEK", // 设备编码
+//				   "payload_index": ["81-0-0"]  // 负载索引，此参数可通过获取设备列表中data.list.drone.camera_list.camera_index传入
+//				}`
+func (F *FH2Adapter) DeleteDeviceControl(projectUuid string, payLoad io.Reader) (string, error) {
+	url := fmt.Sprintf("%s/openapi/v0.1/device/control", config.FH2Settings["host"])
+	resp, err := F.doRequest(context.Background(), http.MethodDelete, url, payLoad, projectUuid)
+	return string(resp), err
+}
 
-// 航线上传
-func (F *FH2Adapter) SetFinishUpload(projectUuid string, objectKeyPrefix string, fileName string) (string, error) {
-	url := fmt.Sprintf("%s/openapi/v0.1/wayline/finish-upload", config.FH2Settings["host"])
-	payload := strings.NewReader(fmt.Sprintf(`{"name":"%s","object_key":"%s"}`, fileName, objectKeyPrefix))
-	resp, err := F.doRequest(context.Background(), http.MethodPost, url, payload, projectUuid)
+// 图传清晰度设置(支持自动，流畅，超清设置)
+// 需要先调用控制权获取接口获取飞行器控制权后才能进行图传清晰度设置
+// camera_index 信息可通过获取组织下的设备列表接口下的 data.list.drone:.camera_list 中获取
+//
+//	payLoad := `{
+//				   "sn": "1581F6Q8D242100CPWEK", // 设备编码
+//				   "camera_index": 81-0-0,  // 相机索引，0表示主相机，1表示辅助相机
+//				   "quality": "ultra_high_definition"   // 清晰度选项，支持 "adaptive"（自动）、"smooth"（流畅）、"ultra_high_definition"（超清）
+//				}`
+func (F *FH2Adapter) UpdateDeviceStreamQuality(projectUuid string, payLoad io.Reader) (string, error) {
+	url := fmt.Sprintf("%s/openapi/v0.1/device/stream/quality", config.FH2Settings["host"])
+	resp, err := F.doRequest(context.Background(), http.MethodPut, url, payLoad, projectUuid)
+	return string(resp), err
+}
+
+// 自定义网络RTK标定
+//
+//	payLoad := `{
+//			   "host": "10.53.226.97",  // 服务器地址
+//			   "port": 8002,   // 端口
+//			   "account": "vntcqkj1CfCO", // 账号
+//			   "password": "Sbii1qoJBows", // 密码
+//			   "mount_point": "RTCM33_GRCEJ" // 挂载点
+//			}`
+func (F *FH2Adapter) CreateDeviceRTK(projectUuid string, deviceSn string, payLoad string) (string, error) {
+	url := fmt.Sprintf("%s/openapi/v0.1/device/%s/rtk", config.FH2Settings["host"], deviceSn)
+	resp, err := F.doRequest(context.Background(), http.MethodPost, url, strings.NewReader(payLoad), projectUuid)
+	return string(resp), err
+}
+
+// 开启直播
+// 启用设备直播功能后，设备将自动向媒体服务器推流，并返回对应直播供应商的鉴权信息。需集成供应商的SDK（如火山引擎、声网、SRS等），通过鉴权信息调用其接口拉取直播流。
+// camera_index 信息可通过获取组织下的设备列表接口下的 data.list.drone.camera_list 中获取。
+// 没有拉流观众5分钟后，将停止直播推流。
+//
+//	payLoad := `{
+//				  "sn": "1581F6Q8D242100CPWEK", // 设备编码
+//				  "camera_index": "81-0-0",  // 相机索引，0表示主相机，1表示辅助相机
+//				  "video_expire": 7200   // 直播推流Token有效期，超过这个有限期直播将中止。
+//				  "quality_type": "adaptive"   // 直播清晰度，adaptive（自动）、smooth（流畅）、ultra_high_definition（超清）
+//				}`
+func (F *FH2Adapter) LiveStreamStart(projectUuid string, payLoad string) (string, error) {
+	url := fmt.Sprintf("%s/openapi/v0.1/live-stream/start", config.FH2Settings["host"])
+	resp, err := F.doRequest(context.Background(), http.MethodPost, url, strings.NewReader(payLoad), projectUuid)
 	return string(resp), err
 }
 
@@ -168,11 +264,13 @@ func (F *FH2Adapter) CreateFlightTask(projectUuid string, payLoad string) (strin
 }
 
 // 更新飞行状态
-
-// 获取飞行任务轨迹信息
-func (F *FH2Adapter) GetFlightTaskTrack(projectUuid string, task_uuid string) (string, error) {
-	url := fmt.Sprintf("%s/openapi/v0.1/flight-task/%s/track", config.FH2Settings["host"], task_uuid)
-	resp, err := F.doRequest(context.Background(), http.MethodGet, url, nil, projectUuid)
+//
+//	payLoad := `{
+//				   "status": restored/restored  //任务挂起&任务恢复
+//				}`
+func (F *FH2Adapter) UpdateFlightTaskStatus(projectUuid string, task_uuid string, payLoad io.Reader) (string, error) {
+	url := fmt.Sprintf("%s/openapi/v0.1/flight-task/%s/status", config.FH2Settings["host"], task_uuid)
+	resp, err := F.doRequest(context.Background(), http.MethodPut, url, payLoad, projectUuid)
 	return string(resp), err
 }
 
@@ -197,10 +295,18 @@ func (F *FH2Adapter) GetFlightTaskMedia(projectUuid string, task_uuid string) (s
 	return string(resp), err
 }
 
-// 开启直播
-func (F *FH2Adapter) LiveStreamStart(projectUuid string, payLoad string) (string, error) {
-	url := fmt.Sprintf("%s/openapi/v0.1/live-stream/start", config.FH2Settings["host"])
-	resp, err := F.doRequest(context.Background(), http.MethodPost, url, strings.NewReader(payLoad), projectUuid)
+// 获取飞行任务轨迹信息
+func (F *FH2Adapter) GetFlightTaskTrack(projectUuid string, task_uuid string) (string, error) {
+	url := fmt.Sprintf("%s/openapi/v0.1/flight-task/%s/track", config.FH2Settings["host"], task_uuid)
+	resp, err := F.doRequest(context.Background(), http.MethodGet, url, nil, projectUuid)
+	return string(resp), err
+}
+
+// 航线上传完成通知
+func (F *FH2Adapter) SetFinishUpload(projectUuid string, objectKeyPrefix string, fileName string) (string, error) {
+	url := fmt.Sprintf("%s/openapi/v0.1/wayline/finish-upload", config.FH2Settings["host"])
+	payload := strings.NewReader(fmt.Sprintf(`{"name":"%s","object_key":"%s"}`, fileName, objectKeyPrefix))
+	resp, err := F.doRequest(context.Background(), http.MethodPost, url, payload, projectUuid)
 	return string(resp), err
 }
 
@@ -222,6 +328,20 @@ func (F *FH2Adapter) GetWayLineInfo(projectUuid string, wayLineUuid string) (str
 func (F *FH2Adapter) CreateModel(projectUuid string, payLoad string) (string, error) {
 	url := fmt.Sprintf("%s/openapi/v0.1/model/create", config.FH2Settings["host"])
 	resp, err := F.doRequest(context.Background(), http.MethodPost, url, strings.NewReader(payLoad), projectUuid)
+	return string(resp), err
+}
+
+// 获取模型详情
+func (F *FH2Adapter) GetModelInfo(projectUuid string, modelId int64) (string, error) {
+	url := fmt.Sprintf("%s/openapi/v0.1/model/%s", config.FH2Settings["host"], modelId)
+	resp, err := F.doRequest(context.Background(), http.MethodGet, url, nil, projectUuid)
+	return string(resp), err
+}
+
+// 获取项目下模型列表
+func (F *FH2Adapter) GetModelList(projectUuid string) (string, error) {
+	url := fmt.Sprintf("%s//openapi/v0.1/model", config.FH2Settings["host"])
+	resp, err := F.doRequest(context.Background(), http.MethodGet, url, nil, projectUuid)
 	return string(resp), err
 }
 
--- service/dji_fh2_adapter.go
+++ service/dji_fh2_adapter.go
@@ -2,6 +2,8 @@
 
 package service
 
+import "io"
+
 // 定义适配器接口
 type FH2DroneAdapter interface {
 	/**  司空2  **/
@@ -12,30 +14,50 @@ type FH2DroneAdapter interface {
 	GetprojectList() (string, error)
 	// 获取项目下的设备列表
 	GetDeviceList(projectUuid string) (string, error)
-	// 获取设备HMS信息
-	GetDeviceHms(projectUuid string, deviceSnList string) (string, error)
 	// 获取项目的存储上传凭证
 	GetProjectStsToken(projectUuid string) (string, error)
 	// 获取设备物模型信息
 	GetStsToken(projectUuid string, deviceSn string) (string, error)
-	// 航线上传
-	SetFinishUpload(projectUuid string, objectKeyPrefix string, fileName string) (string, error)
+	// 获取设备HMS信息
+	GetDeviceHms(projectUuid string, deviceSnList string) (string, error)
+	// 实时控制指令下发
+	UpdateDeviceCommand(projectUuid string, deviceSn string, payLoad io.Reader) (string, error)
+	// 机场相机切换
+	UpdateDeviceChangeCamera(projectUuid string, payLoad io.Reader) (string, error)
+	// 设备飞行器镜头切换
+	UpdateDeviceChangeLens(projectUuid string, payLoad io.Reader) (string, error)
+	// 获取设备控制权
+	GetDeviceControl(projectUuid string, payLoad io.Reader) (string, error)
+	// 设备控制权释放
+	DeleteDeviceControl(projectUuid string, payLoad io.Reader) (string, error)
+	// 图传清晰度设置
+	UpdateDeviceStreamQuality(projectUuid string, payLoad io.Reader) (string, error)
+	// 自定义网络RTK标定
+	CreateDeviceRTK(projectUuid string, deviceSn string, payLoad string) (string, error)
+	// 开启直播
+	LiveStreamStart(projectUuid string, payLoad string) (string, error)
+	// 创建飞行任务
+	CreateFlightTask(projectUuid string, payLoad string) (string, error)
+	// 更新飞行状态
+	UpdateFlightTaskStatus(projectUuid string, task_uuid string, payLoad io.Reader) (string, error)
+	// 获取飞行任务信息
+	GetFlightTaskInfo(projectUuid string, task_uuid string) (string, error)
 	// 获取飞行任务列表
 	GetFlightTask(projectUuid string, sn string, name string, begin_at int, end_at int, task_type string, status string) (string, error)
 	// 获取飞行任务产生的媒体资源
 	GetFlightTaskMedia(projectUuid string, task_uuid string) (string, error)
-	// 开启直播
-	LiveStreamStart(projectUuid string, payLoad string) (string, error)
 	// 获取飞行任务轨迹信息
 	GetFlightTaskTrack(projectUuid string, task_uuid string) (string, error)
-	// 获取飞行任务信息
-	GetFlightTaskInfo(projectUuid string, task_uuid string) (string, error)
-	// 创建飞行任务
-	CreateFlightTask(projectUuid string, payLoad string) (string, error)
+	// 航线上传完成通知
+	SetFinishUpload(projectUuid string, objectKeyPrefix string, fileName string) (string, error)
 	// 获取项目下航线列表
 	GetWayLine(projectUuid string) (string, error)
 	// 获取项目下的航线详情
 	GetWayLineInfo(projectUuid string, wayLineUuid string) (string, error)
 	// 模型重建
 	CreateModel(projectUuid string, payLoad string) (string, error)
+	// 获取模型详情
+	GetModelInfo(projectUuid string, modelId int64) (string, error)
+	// 获取项目下的模型列表
+	GetModelList(projectUuid string) (string, error)
 }
